38b0c2b7261c06bf9ae5821123829e22032b7136
lamtruong <76791917+lamtrg279@users.noreply.github.com>
2024-02-26 13:27:18 -0600

    Accordion

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,23 +1,63 @@
-import {useState } from 'react'
-import data from '../accordion/data.js'
+import { useState } from "react";
+import data from "../accordion/data.js";
+import "./styles.css";
 
 export default function Accordion() {
-  const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState(null);
+  const [multiSelection, setMultiSelection] = useState([]);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
 
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
 
-  return <div className='wrapper'>
-  <div className='accordion'>
-    {
-      data && data.length > 0 ? 
-      data.map(dataItem => 
-        <div className='item'>
-          <div className='title'>
-            <h3>{dataItem.question}</h3>
-            <span>+</span>
-          </div>
-        </div>
-        ) : <div>No data found</div>
+  function handleMultipleSelection(getCurrentId) {
+    let copyMultiple = [...multiSelection]    // copy multiple selection, we don't want to mess with original array
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId)
+
+    if (findIndexOfCurrentId === -1) { // indexOf = -1 means current is not in the array
+      copyMultiple.push(getCurrentId)
     }
-  </div>
-  </div>
+    else {
+      copyMultiple.splice(findIndexOfCurrentId, 1) // splice is used for delete item from the array, this line means remove with the index (findIndexOfCUrrent is the index), and remove 1 item
+    }
+    setMultiSelection(copyMultiple)
+  }
+
+  return (
+    <div className='acc-wrapper'>
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className='accordion'>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className='item'>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultipleSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className='title'
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection ? multiSelection.indexOf(dataItem.id) !== -1 && (
+                <div className="content">{dataItem.answer}</div>
+              ) : selected === dataItem.id && (
+                <div className="content">{dataItem.answer}</div>
+              )}
+              {/* {selected === dataItem.id || multiSelection.indexOf(dataItem.id) !== -1 ? (
+                <div className='content'>{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
 }
