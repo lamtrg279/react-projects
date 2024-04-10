0dd16ca8c34426a1d71e03f5f1c76ed343fa9bb9
lamtruong <nl2791999@gmail.com>
2024-04-10 10:55:06 -0500

    tree-view

--- src/components/tree-view/index.jsx
+++ src/components/tree-view/index.jsx
@@ -1,5 +1,6 @@
 import MenuList from "./menu-list";
+import './styles.css';
 
 export default function TreeView({ menus = [] }) {
-  return <div className='tree-view=container'>{<MenuList list={menus} />}</div>;
+  return <div className='tree-view-container'>{<MenuList list={menus} />}</div>;
 }
--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 import MenuList from "./menu-list";
+import {FaMinus, FaPlus} from 'react-icons/fa'
 
 export default function MenuItem({ item }) {
   const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
-  function handleToggleChildren(getCurrentLabel) {
+  function handleToggleChildren(getCurrentlabel) {
     setDisplayCurrentChildren({
       ...displayCurrentChildren,
-      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel]
+      [getCurrentlabel]: !displayCurrentChildren[getCurrentlabel],
     });
   }
 
+  console.log(displayCurrentChildren);
+
   return (
     <li>
-      <div style={{ display: "flex", gap: "20px" }}>
+      <div className="menu-item">
         <p>{item.label}</p>
         {item && item.children && item.children.length ? (
-          <span onClick={() => handleToggleChildren(item.labe)}>+</span>
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {
+                displayCurrentChildren[item.label] ? <FaMinus color="#fff" size={25} /> : <FaPlus color="#fff" size={25} />
+            }
+          </span>
         ) : null}
       </div>
-      <p>{item.label}</p>
-      {item && item.children && item.children.length > 0 ? (
+
+      {item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
